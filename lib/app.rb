9d6d61ec358d0d65b827c1ec31aecd00b5e6adc2
samover <samuel.overloop@gmail.com>
2015-10-26 16:42:07 +0000

    challenge 7: added extra routes

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
-get '/:name' do
-  "Hello! #{params[:name]}!!!"
+get '/' do
+  "Hello World!"
+end
+
+get '/secret' do
+  "Sssssh. This a secret page."
+end
+
+get '/greetings/:name' do
+  "Hello #{params[:name]}"
 end
